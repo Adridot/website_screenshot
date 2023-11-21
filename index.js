b988a67d73a336926d8e54469f7a13fd382f28dd
Adrien Didot <adrien.didot@gmail.com>
2023-11-21 17:12:44 +0100

    add more logging

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-import express from 'express';
-import puppeteer from 'puppeteer-core';
-import fs from 'fs';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const app = express();
-
-app.use(express.json());
-
-// Set up a route to capture a specific element from a website
 app.get('/capture', async (req, res) => {
     const { url, elementSelector } = req.query;
+
+    console.log(`Capturing screenshot from ${url} with selector ${elementSelector}`);
 
     if (!url) {
         return res.status(400).send('URL is required');
@@ -27,18 +15,17 @@ app.get('/capture', async (req, res) => {
         const page = await browser.newPage();
         await page.goto(url);
         const element = await page.$(elementSelector);
+        if (!element) {
+            console.error(`Element not found with selector ${elementSelector}`);
+            return res.status(500).send('Element not found');
+        }
         const screenshot = await element.screenshot();
         await browser.close();
 
         fs.writeFileSync('screenshot.png', screenshot);
         res.sendFile('screenshot.png', { root: __dirname });
     } catch (error) {
-        console.error(error);
+        console.error(`Error capturing screenshot: ${error}`);
         res.status(500).send('Error capturing screenshot');
     }
-});
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
 });
