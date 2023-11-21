a75bc401d11bdda08038610c357bbad00a6419bc
Adrien Didot <adrien.didot@gmail.com>
2023-11-21 17:22:27 +0100

    try changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import puppeteer from 'puppeteer-core';
 import fs from 'fs';
-import {fileURLToPath} from 'url';
-import {dirname} from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -15,31 +15,24 @@ app.use(express.json());
 app.get('/capture', async (req, res) => {
     const { url, elementSelector } = req.query;
 
-    console.log(`Capturing screenshot from ${url} with selector ${elementSelector}`);
-
     if (!url) {
         return res.status(400).send('URL is required');
     }
 
     try {
         const browser = await puppeteer.launch({
-            executablePath: '/usr/bin/chromium', // use installed Chromium
             args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
         const page = await browser.newPage();
         await page.goto(url);
         const element = await page.$(elementSelector);
-        if (!element) {
-            console.error(`Element not found with selector ${elementSelector}`);
-            return res.status(500).send('Element not found');
-        }
         const screenshot = await element.screenshot();
         await browser.close();
 
         fs.writeFileSync('screenshot.png', screenshot);
         res.sendFile('screenshot.png', { root: __dirname });
     } catch (error) {
-        console.error(`Error capturing screenshot: ${error}`);
+        console.error(error);
         res.status(500).send('Error capturing screenshot');
     }
 });
